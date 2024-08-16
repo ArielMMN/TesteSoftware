e8d13ef73459f31c77fd15651976b71f3e7cb7a1
ArielMMN <arielmmoraes@gmail.com>
2024-08-16 09:26:21 -0300

    Adicionei mais três testes

--- testes-e2e-cypress-main/cypress/e2e/inicio.cy.js
+++ testes-e2e-cypress-main/cypress/e2e/inicio.cy.js
@@ -1,6 +1,32 @@
 describe('Página Principal', () => {
+  beforeEach( () => {
+    cy.visit('http://localhost:3000/')
+  })
+  
   it('Deve renderizar h1 com o texto correto!', () => {
     cy.visit('http://localhost:3000')
-    cy.get('[data-test="titulo-principal"]').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
+    cy.getByData('titulo-principal').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
+  })
+})
+
+describe('Página Principal', () => {
+  beforeEach( () => {
+    cy.visit('http://localhost:3000/')
+  })
+  
+  it('Deve renderizar h2 com o texto correto!', () => {
+    cy.visit('http://localhost:3000')
+    cy.getByData('titulo-sub').contains('Vantagens do nosso banco:')
+  })
+})
+
+describe('Página Principal', () => {
+  beforeEach( () => {
+    cy.visit('http://localhost:3000/')
+  })
+  
+  it('Deve renderizar p com o texto correto!', () => {
+    cy.visit('http://localhost:3000')
+    cy.getByData('paragrafo-principal').contains('Isso mesmo, nossa conta é digital, sem custo fixo e mais que isso: sem tarifa de manutenção.')
   })
 })
--- testes-e2e-cypress-main/src/paginas/Inicio/index.jsx
+++ testes-e2e-cypress-main/src/paginas/Inicio/index.jsx
@@ -22,12 +22,14 @@ export default function Inicio() {
             />
           </div>
           <div className={estilos.vantagens}>
-            <h2>Vantagens do nosso banco:</h2>
+            <h2 data-test="titulo-sub">
+              Vantagens do nosso banco:
+            </h2>
             <div className={estilos.vantagens__itens}>
               <div className={estilos.vantagens__item}>
                 <img src={presente} alt="ícone de um presente" />
                 <h3>Conta e cartão gratuitos</h3>
-                <p>
+                <p data-test="paragrafo-principal">
                   Isso mesmo, nossa conta é digital, sem custo fixo e mais que
                   isso: sem tarifa de manutenção.
                 </p>
