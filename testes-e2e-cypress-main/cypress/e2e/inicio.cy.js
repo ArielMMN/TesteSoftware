f52aaf9d32185d9bf310ba96e6eb4cfec965064f
ArielMMN <arielmmoraes@gmail.com>
2024-08-16 14:18:17 -0300

    Adicionei todos os testes restantes na pagina inicial

--- testes-e2e-cypress-main/cypress/e2e/inicio.cy.js
+++ testes-e2e-cypress-main/cypress/e2e/inicio.cy.js
@@ -27,6 +27,84 @@ describe('Página Principal', () => {
   
   it('Deve renderizar p com o texto correto!', () => {
     cy.visit('http://localhost:3000')
-    cy.getByData('paragrafo-principal').contains('Isso mesmo, nossa conta é digital, sem custo fixo e mais que isso: sem tarifa de manutenção.')
+    cy.getByData('paragrafo-cartao').contains('Isso mesmo, nossa conta é digital, sem custo fixo e mais que isso: sem tarifa de manutenção.')
+  })
+})
+
+describe('Página Principal', () => {
+  beforeEach( () => {
+    cy.visit('http://localhost:3000/')
+  })
+  
+  it('Deve renderizar h3 com o texto correto!', () => {
+    cy.visit('http://localhost:3000')
+    cy.getByData('legenda-cartao').contains('Conta e cartão gratuitos')
+  })
+})
+
+describe('Página Principal', () => {
+  beforeEach( () => {
+    cy.visit('http://localhost:3000/')
+  })
+  
+  it('Deve renderizar h3 com o texto correto!', () => {
+    cy.visit('http://localhost:3000')
+    cy.getByData('legenda-saque').contains('Saques sem custo')
+  })
+})
+
+
+describe('Página Principal', () => {
+  beforeEach( () => {
+    cy.visit('http://localhost:3000/')
+  })
+  
+  it('Deve renderizar p com o texto correto!', () => {
+    cy.visit('http://localhost:3000')
+    cy.getByData('paragrafo-saque').contains('Você pode sacar gratuitamente 4x por mês de qualquer Banco 24h.')
+  })
+})
+
+describe('Página Principal', () => {
+  beforeEach( () => {
+    cy.visit('http://localhost:3000/')
+  })
+  
+  it('Deve renderizar h3 com o texto correto!', () => {
+    cy.visit('http://localhost:3000')
+    cy.getByData('legenda-ponto').contains('Programa de pontos')
+  })
+})
+
+describe('Página Principal', () => {
+  beforeEach( () => {
+    cy.visit('http://localhost:3000/')
+  })
+  
+  it('Deve renderizar p com o texto correto!', () => {
+    cy.visit('http://localhost:3000')
+    cy.getByData('paragrafo-ponto').contains('Você pode acumular pontos com suas compras no crédito sem pagar mensalidade!')
+  })
+})
+
+describe('Página Principal', () => {
+  beforeEach( () => {
+    cy.visit('http://localhost:3000/')
+  })
+  
+  it('Deve renderizar h3 com o texto correto!', () => {
+    cy.visit('http://localhost:3000')
+    cy.getByData('legenda-seguro').contains('Seguro Dispositivos')
+  })
+})
+
+describe('Página Principal', () => {
+  beforeEach( () => {
+    cy.visit('http://localhost:3000/')
+  })
+  
+  it('Deve renderizar p com o texto correto!', () => {
+    cy.visit('http://localhost:3000')
+    cy.getByData('paragrafo-seguro').contains('Seus dispositivos móveis (computador e laptop) protegidos por uma mensalidade simbólica.')
   })
 })
--- testes-e2e-cypress-main/src/paginas/Inicio/index.jsx
+++ testes-e2e-cypress-main/src/paginas/Inicio/index.jsx
@@ -28,24 +28,26 @@ export default function Inicio() {
             <div className={estilos.vantagens__itens}>
               <div className={estilos.vantagens__item}>
                 <img src={presente} alt="ícone de um presente" />
-                <h3>Conta e cartão gratuitos</h3>
-                <p data-test="paragrafo-principal">
+                <h3 data-test="legenda-cartao">
+                  Conta e cartão gratuitos
+                </h3>
+                <p data-test="paragrafo-cartao">
                   Isso mesmo, nossa conta é digital, sem custo fixo e mais que
                   isso: sem tarifa de manutenção.
                 </p>
               </div>
               <div className={estilos.vantagens__item}>
                 <img src={saque} alt="ícone de uma mão segurando um dinheiro" />
-                <h3>Saques sem custo</h3>
-                <p>
+                <h3 data-test="legenda-saque">Saques sem custo</h3>
+                <p data-test="paragrafo-saque">
                   Você pode sacar gratuitamente 4x por mês de qualquer Banco
                   24h.
                 </p>
               </div>
               <div className={estilos.vantagens__item}>
                 <img src={estrela} alt="ícone de uma estrela" />
-                <h3>Programa de pontos</h3>
-                <p>
+                <h3 data-test="legenda-ponto">Programa de pontos</h3>
+                <p data-test="paragrafo-ponto">
                   Você pode acumular pontos com suas compras no crédito sem
                   pagar mensalidade!
                 </p>
@@ -55,8 +57,8 @@ export default function Inicio() {
                   src={dispositivo}
                   alt="ícone de um dispositivo como notebook"
                 />
-                <h3>Seguro Dispositivos</h3>
-                <p>
+                <h3 data-test="legenda-seguro">Seguro Dispositivos</h3>
+                <p data-test="paragrafo-seguro">
                   Seus dispositivos móveis (computador e laptop) protegidos por
                   uma mensalidade simbólica.
                 </p>
