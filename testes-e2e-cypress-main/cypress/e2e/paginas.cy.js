e41ead4cd229b18607ff21da8c93775be42a47d7
ArielMMN <arielmmoraes@gmail.com>
2024-08-23 11:20:36 -0300

    Teste de direcionamento de páginas

--- testes-e2e-cypress-main/cypress/e2e/paginas.cy.js
+++ testes-e2e-cypress-main/cypress/e2e/paginas.cy.js
@@ -1,12 +1,19 @@
+
 describe('Testando múltiplas páginas', () => {
     it('Deve conseguir acessar a página de cartões', () => {
-    cy.visit('/')
-    cy.getByData('botao-login').click()
-    cy.getByData('email-input').type('coelho1@email.com')
-    cy.getByData('senha-input').type('456789')
-    cy.getByData('botao-enviar').click()
-    cy.getByData('app-home').find('a').eq(1).click()
-    cy.getByData('titulo-cartoes').should('exist').and('have.text', 'Meus cartões')
-    })    
+        cy.visit('http://localhost:3000')
+        cy.getByData('botao-login').click()
+        cy.getByData('email-input').type('neilton@alura.com')
+        cy.getByData('senha-input').type('123456')
+        cy.getByData('botao-enviar').click()
+        cy.location('pathname').should('eq', '/home')
+        cy.getByData('app-home').find('a').eq(1).click()
+        cy.getByData('titulo-cartoes').should('exist').and('have.text', 'Meus cartões')
+        cy.location('pathname').should('eq', '/home/cartoes')
+        cy.getByData('app-home').find('a').eq(2).click()
+        cy.location('pathname').should('eq', '/home/servicos')
+        cy.getByData('app-home').find('a').eq(3).click()
+        cy.location('pathname').should('eq', '/home/investimentos')
+        
+    })
 })
-
